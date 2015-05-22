6b7c0849e4dc1fc2610ceb46355a125240980a68
幻影集団 <green0014jp@yahoo.co.jp>
2015-05-23 00:20:45 +0900

    ただの現時刻の表示に変更

--- lib/knockknock.rb
+++ lib/knockknock.rb
@@ -3,9 +3,9 @@ require "active_support"
 
 module Knockknock
   def self.knock_clock
-    dt = DateTime.now
-    puts dt
-    sleep 1
-    knock_clock
+    DateTime.now
+    # puts dt
+    # sleep 1
+    # knock_clock
   end
 end
