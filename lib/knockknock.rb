ede1df0ffb6d39c38bbabcde4f9fae383ed5d41f
幻影集団 <green0014jp@yahoo.co.jp>
2015-05-22 22:34:51 +0900

    add knock_clock

--- lib/knockknock.rb
+++ lib/knockknock.rb
@@ -1,4 +1,12 @@
 require "knockknock/engine"
+require "active_support/all"
+require 'date'
 
 module Knockknock
+  def self.knock_clock
+    dt = DateTime.now
+    puts dt
+    sleep 1
+    knock_clock
+  end
 end
